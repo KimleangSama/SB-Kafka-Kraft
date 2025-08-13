30d82b410599b5407dfd0f42b9501d5fbaedd393
Kea Kimleang <kimleang.prof@gmail.com>
2025-08-13 11:38:29 +0700

    feat: improve

--- src/main/java/com/keakimleang/demokafka/DemoController.java
+++ src/main/java/com/keakimleang/demokafka/DemoController.java
@@ -1,5 +1,6 @@
 package com.keakimleang.demokafka;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,17 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class DemoController {
+    private final String site;
     private final KafkaProducerService producerService;
 
     public DemoController(KafkaProducerService producerService) {
         this.producerService = producerService;
+        site = System.getenv().getOrDefault("SITE", "NONE");
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "Hello from " + site + " backend!";
     }
 
     @PostMapping("/send")
